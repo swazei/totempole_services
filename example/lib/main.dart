137116bcb611d707db5694c689287e87d2e33d13
Shiba Kar <shibakar@swazei.com>
2023-08-21 20:21:48 +0530

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:totempole_services/models/app_notification.dart';
 import 'package:totempole_services/services/services.dart';
 
 Future<void> start() async {
   final client = Client();
-  final users = Users(client);
+  // final users = Users(client);
   final database = Databases(client);
-  final storage = Storage(client);
+  // final storage = Storage(client);
   var env = DotEnv(includePlatformEnvironment: true)..load();
   client
       .setEndpoint(env['URL'] ?? "")
@@ -18,14 +19,24 @@ Future<void> start() async {
   // final usersService = UsersService(users: users);
   // final totempoleService = TotempoleService(database: database);
   //final profileService = ProfileService(database: database);
-  final usernameService = UsernameService(database: database);
+  //final usernameService = UsernameService(database: database);
+  final notificationService = NotificationService(database: database);
+  for (var i = 0; i < 5; i++) {
+    final no = AppNotification(
+      title: "This is a test",
+      from: "641d91e4047127dd6956",
+      to: "6423e690ce5e7fd7ecd4",
+      type: Type.friendRequest,
+    );
+    notificationService.createNotification(no);
+  }
   //final totempoleStorage = TotempoleStorageService(storage: storage);
 //  await friendsService.createFakeFriendList(alltheUsers, 65);
   // final member = MemberService(totempoleID: '', database: database);
   // final x = await member.deleteMembersRelatedToToempole();
   // var x = await totempoleService.getAllTotempoles();
   // await profileService.deleteAllProfiles();
-  await usernameService.deleteAllUsernames();
+  //await usernameService.deleteAllUsernames();
   // await totempoleService.deleteAllTotempoles();
   //totempoleStorage.deleteAllTotemPoleAvtars();
   // await friendsService.deleteAllFriends();

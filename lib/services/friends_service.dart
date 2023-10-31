d5b6ac17141be596e8e5cd4a96c29ccd015d8524
Shiba Kar <shibakar@swazei.com>
2023-10-31 12:55:54 +0530

    updated friend service

--- lib/services/friends_service.dart
+++ lib/services/friends_service.dart
@@ -8,25 +8,26 @@ import '../models/models.dart';
 class FriendService {
   final Databases database;
   FriendService({required this.database});
-  Future<FriendList> getFirstFriendList() async {
+  Future<FriendList> getFirstFriendList([List<String>? queries]) async {
     final friends = await database.listDocuments(
-      databaseId: '634d7cac5a7a4ada2259',
-      collectionId: '638edb445afbc67c4e1f',
-    );
+        databaseId: '634d7cac5a7a4ada2259',
+        collectionId: '638edb445afbc67c4e1f',
+        queries: queries);
     var f = friends.documents.map((e) => Friend.fromJson(e.data)).toList();
     return FriendList(friends: f, total: friends.total);
   }
 
-  Future<List<Friend>> paginatedFriendsList([int offset = 0]) async {
+  Future<List<Friend>> paginatedFriendsList(
+      [int offset = 0, List<String>? queries]) async {
     final friends = await database.listDocuments(
       databaseId: '634d7cac5a7a4ada2259',
       collectionId: '638edb445afbc67c4e1f',
-      queries: [Query.offset(offset)],
+      queries: [...queries ?? [], Query.offset(offset)],
     );
     return friends.documents.map((e) => Friend.fromJson(e.data)).toList();
   }
 
-  Future<List<Friend>> getAllFriends() async {
+  Future<List<Friend>> getAllFriends([List<String>? queries]) async {
     final totalFiends = await getFirstFriendList();
     final List<Friend> tempFriends = [];
     tempFriends.addAll(totalFiends.friends);
@@ -35,24 +36,31 @@ class FriendService {
     int pages = totalFiends.total ~/ limit;
     for (var i = 0; i < pages; i++) {
       offset = offset + limit;
-      final data = await paginatedFriendsList(offset);
+      final data = await paginatedFriendsList(offset, queries);
       tempFriends.addAll(data);
       print(i);
     }
     return tempFriends;
   }
 
+  Future<List<Friend>> getAllFriendFromRequests(String fromUserId) async {
+    final q = [Query.equal('requested_from_user_id', fromUserId)];
+    return await getAllFriends(q);
+  }
+
+  Future<dynamic> deleteFriend(String friendId) async {
+    return await database.deleteDocument(
+      databaseId: '634d7cac5a7a4ada2259',
+      collectionId: '638edb445afbc67c4e1f',
+      documentId: friendId,
+    );
+  }
+
   Future<void> deleteAllFriends() async {
     final friends = await getAllFriends();
 
-    print(friends.length);
-
     for (var friend in friends) {
-      await database.deleteDocument(
-        databaseId: '634d7cac5a7a4ada2259',
-        collectionId: '638edb445afbc67c4e1f',
-        documentId: friend.id!,
-      );
+      await deleteFriend(friend.id!);
     }
   }
 

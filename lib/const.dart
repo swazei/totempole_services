1d8d054ffdacfe010bb1457639e07e6010065e4c
Manisha Dharmik <manisha@swazei.com>
2025-02-01 23:07:07 +0530

    Updated constants for Production

--- lib/const.dart
+++ lib/const.dart
@@ -1,9 +1,20 @@
-const String developmentDB = '656ed306a07979f7ed59';
-const String developmentStorage = '65706cc9b542d611c6ec';
+// [DEVELOPMENT]
+// const String developmentDB = '656ed306a07979f7ed59'; // Dev DB
+// const String profileCOL = '634d7cac5a7a4ada2259';    // Profile Collection
+// const String localBussCol = '65c2017748db89481e3a';  // Local bussiness Collection
+// const String developmentStorage = '6794b8410022011acd60';
 
+
+// [PRODUCTION]
+const String developmentDB = '656ed31b3edbb2e72cba'; // Prod DB
+const String profileCOL = '6794c1d1002c407c3ff3';    // Profile Collection
+const String localBussCol = '6794c2d200092d9febd3';  // Local bussiness Collection
+const String developmentStorage = '6794bb5c0028e5d38504';
+
+
+// const String developmentStorage = '65706cc9b542d611c6ec';
 const String usernameCOL = '656f143c9f061be5db07';
 const String friendsCOL = '656f140b22d9772841fa';
 const String totempoleCOL = '6351859dee3a309dc94a';
-const String profileCOL = '634d7cac5a7a4ada2259';
-const String notificationsCOL = '6392b787aa1122e2afc3';
 const String memberCOL = '656f11056a8d40f59b6e';
+const String notificationsCOL = '6392b787aa1122e2afc3';
